5da8cd1c59be7fe79faf055b00fda3695ed68648
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-22 19:58:32 +0530

    updating config File with Browser

--- playwright.config.js
+++ playwright.config.js
@@ -41,12 +41,12 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    /*{
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+    /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
