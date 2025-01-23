67a76f1a48e2f142d719bccaa701eaeb0b984d45
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-23 21:52:32 +0530

    updating login scenarios with external data File

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,19 +1,19 @@
 const {test,expect}=require('@playwright/test');
 const {POManager}=require('../pageObject/POManager');
-//cnsttt dataset=(JSON.parse(JSON.stringify(require('../utils/login.json'))));
+const dataset=JSON.parse(JSON.stringify(require('../utils/login.json')));
 
-test('@smoke valid LogIn',async({page})=>
+for(const credentials of dataset)
 {
-    
-    const username ="sumitkoley727@gmail.com";
-    const password ="Sumit@123";
+test(`@smoke valid LogIn for ${credentials.username}`,async({page})=>
+{
     const textContent = page.locator("div[class='left mt-1'] h3");
+    const invalidLoginMessage =page.locator("div[aria-label='Incorrect email or password.']");
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage(); 
-    loginPage.LogIn(username,password);
-    await expect(textContent).toHaveText("Automation");
-})
-test('Invalid LogIn',async({page})=>
+    loginPage.LogIn(credentials.username,credentials.password);
+    await expect.soft(textContent).toHaveText("Automation");
+});
+test(`@smoke Invalid LogIn for ${credentials.username}`,async({page})=>
 {
     const username ="sumitkoley727@gmail.comm";
     const password ="Sumit@1233";
@@ -23,3 +23,4 @@ test('Invalid LogIn',async({page})=>
     loginPage.LogIn(username,password);
     await expect(invalidLoginMessage).toHaveText("Incorrect email or password."); 
 })
+};
