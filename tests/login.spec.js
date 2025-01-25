7e39a72a035d612be4387622614e4954728d01eb
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-25 06:50:14 +0530

    Removing unwanted locators from test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,20 +7,18 @@ for(const credentials of dataset)
 test(`@smoke valid LogIn for ${credentials.username}`,async({page})=>
 {
     const textContent = page.locator("div[class='left mt-1'] h3");
-    const invalidLoginMessage =page.locator("div[aria-label='Incorrect email or password.']");
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage(); 
     loginPage.LogIn(credentials.username,credentials.password);
-    await expect.soft(textContent).toHaveText("Automation");
+    await expect(textContent).toHaveText("Automation");
 });
 test(`@smoke Invalid LogIn for ${credentials.username}`,async({page})=>
 {
-    const username ="sumitkoley727@gmail.comm";
-    const password ="Sumit@1233";
+
     const invalidLoginMessage =page.locator("div[aria-label='Incorrect email or password.']");
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage();
-    loginPage.LogIn(username,password);
+    loginPage.LogIn(credentials.username,credentials.password);
     await expect(invalidLoginMessage).toHaveText("Incorrect email or password."); 
 })
 };
