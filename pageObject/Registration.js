da2501fbe777e487d79defe773f4e71fdde77561
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-27 22:54:43 +0530

    Updating stand alone test for registration a

---
+++ pageObject/Registration.js
@@ -0,0 +1,9 @@
+class Registration
+{
+    constructor(page)
+    {
+       this.page = page;
+
+    }
+}
+module.exports={Registration};
---
+++ tests/register.spec.js
@@ -0,0 +1,34 @@
+const {test,expect}=require('@playwright/test');
+const dataset = JSON.parse(JSON.stringify(require('../utils/registration.json')));
+for(const data of dataset)
+{
+test('Register with valid Email',async({page})=>
+{
+    const registerBtn = page.locator(".btn1");
+    const firstName = page.locator("#firstName");
+    const lastName = page.locator("#lastName");
+    const email = page.locator("#userEmail");
+    const mobileNo = page.locator("#userMobile");
+    const gender = page.locator("input[value='Male']");
+    const userPassword = page.locator("#userPassword");
+    const confirmPassword = page.locator("#confirmPassword");
+    const checkBox = page.locator(".col-md-1");
+    const submit = page.locator("#login");
+    const successValidation = page.locator(".headcolor");
+
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await registerBtn.click();
+    await firstName.fill(data.firstName);
+    await lastName.fill(data.lastName);
+    await email.fill(data.email);
+    await mobileNo.fill(data.mobileNo);
+    await page.selectOption(".custom-select","Engineer");
+    await gender.click();
+    await userPassword.fill(data.userPassword);
+    await confirmPassword.fill(data.confirmPassword);
+    await checkBox.click();
+    await submit.click();
+    await expect(successValidation).toHaveText(data.successMessage);
+    
+
+})}
