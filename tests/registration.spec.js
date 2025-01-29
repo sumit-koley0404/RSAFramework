b08df7b93c1a010c884fe70f9fc2f93f201cb3ee
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-29 21:07:12 +0530

    update one script in package.json file

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -3,7 +3,7 @@ const {POManager} = require('../pageObject/POManager');
 const dataset = JSON.parse(JSON.stringify(require('../utils/registration.json')));
 for(const userdata of dataset)
 {
-test.only(` Registration with Valid ${userdata.firstname}`,async({page})=>
+test(`@regstn Registration with Valid ${userdata.firstname}`,async({page})=>
 {
     
   const successValidation = page.locator(".headcolor");
@@ -12,4 +12,5 @@ test.only(` Registration with Valid ${userdata.firstname}`,async({page})=>
   await registrationPage.registration(userdata.firstname,userdata.lastname,userdata.email,
       userdata.mobileno,userdata.userpassword,userdata.confirmpassword);
   await expect(successValidation).toHaveText("Account Created Successfully");
+  page.waitForSelector
 })}
